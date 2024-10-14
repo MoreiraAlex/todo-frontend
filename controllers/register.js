d35d52bf7255137974d7d79a196dabe4f0feb0ea
Alex Moreira <amoreira@allianceconsultoria.com.br>
2024-10-14 10:30:31 -0300

    Implementando requisição do register

---
+++ controllers/register.js
@@ -0,0 +1,37 @@
+document.getElementById('register-form').addEventListener('submit', async function (e) {
+    e.preventDefault();
+  
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+  
+    if (!username || !password) {
+      alert('Por favor, preencha todos os campos.');
+      return;
+    }
+  
+    try {
+      const response = await fetch('http://localhost:3000/auth/register', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          username,
+          password
+        }),
+      });
+  
+      const data = await response.json();
+  
+      if (response.ok) {
+        alert('Registro realizado com sucesso! Agora você pode fazer login.');
+        window.location.href = 'login.html';
+      } else {
+        alert(`Erro: ${data.message}`);
+      }
+    } catch (error) {
+      alert('Erro ao registrar usuário, tente novamente mais tarde.');
+      console.error('Erro:', error);
+    }
+  });
+  
